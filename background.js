c370d807f632d4a2cf4f42c1e5b632b60b45c071
Zhang Bin <zncoder@gmail.com>
2020-08-05 09:00:25 -0700

    initial commit

---
+++ background.js
@@ -0,0 +1,50 @@
+let tabWin = 0
+
+function handleMoveTabs() {
+  if (tabWin) {
+    moveTabs()
+  } else {
+    setTabWin()
+  }
+}
+
+function currentWin() {
+  return new Promise(resolve => {
+    chrome.windows.getCurrent({}, win => { resolve(win.id) })
+  })
+}
+
+function queryTabs(arg) {
+  return new Promise(resolve => {
+    chrome.tabs.query(arg, tabs => { resolve(tabs) })
+  })
+}
+
+async function moveTabs() {
+  let src = tabWin
+  tabWin = 0
+  chrome.browserAction.setBadgeText({})
+  if (!src) {
+    return
+  }
+  
+  let tabs = await queryTabs({highlighted: true, windowId: src})
+  if (tabs.length === 1) {
+    // move all tabs if none other than the active one is highlighted
+    tabs = await queryTabs({windowId: src})
+  }
+  let ids = tabs.map(x => x.id)
+  let dst = await currentWin()
+  chrome.tabs.move(ids, {windowId: dst, index: -1})
+}
+
+async function setTabWin() {
+  tabWin = await currentWin()
+  chrome.browserAction.setBadgeText({text: "!"})
+}
+
+function init() {
+  chrome.browserAction.onClicked.addListener(handleMoveTabs)
+}
+
+init()
