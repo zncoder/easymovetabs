1f96133f05083c3adc7b2669de176b76ada419d3
Bin Zhang <binz@stripe.com>
2020-08-27 17:40:25 -0700

    pop highlighted tabs to their own window

--- background.js
+++ background.js
@@ -8,6 +8,12 @@ function handleMoveTabs() {
   }
 }
 
+function moveToNewWin(tid) {
+  return new Promise(resolve => {
+    chrome.windows.create({tabId: tid}, win => { resolve(win.id) })
+  })
+}
+
 function currentWin() {
   return new Promise(resolve => {
     chrome.windows.getCurrent({}, win => { resolve(win.id) })
@@ -27,11 +33,18 @@ async function moveTabs() {
   if (!src) {
     return
   }
-  
+
   let tabs = await queryTabs({highlighted: true, windowId: src})
   let ids = tabs.map(x => x.id)
   let dst = await currentWin()
-  chrome.tabs.move(ids, {windowId: dst, index: -1})
+  if (dst == src) {
+    // create a new window to move tabs
+    dst = await moveToNewWin(ids[0])
+    ids.shift()
+  }
+  if (ids.length > 0) {
+    chrome.tabs.move(ids, {windowId: dst, index: -1})
+  }
 }
 
 async function setTabWin() {
