2f34e670b77838acbb720eeb7f3d2687c7f841d4
Bin Zhang <zhangbin@gmail.com>
2024-09-04 02:42:21 -0700

    refactor to browser and simplify

--- background.js
+++ background.js
@@ -1,98 +1,63 @@
 let tabWin = 0
 
 async function handleMoveTabs() {
-	let moved = await moveTabs()
-	if (!moved) {
-		setTabWin()
+	if (tabWin !== 0) {
+		try {
+			await browser.windows.get(tabWin)
+		} catch (e) {
+			tabWin = 0
+		}
 	}
+
+	if (tabWin === 0) {
+		await moveOrSetTabWin()
+	} else {
+		await moveFromTabWin()
+	}
+}
+
+async function moveOrSetTabWin() {
+	let wins = await visibleWindows()
+	if (wins.length > 2) {
+		await setTabWin()
+	} else if (wins.length === 2) {
+		await moveTabToAnother(wins)
+	} else {
+		await moveTabToNew()
+	}
+}
+
+async function moveTabToAnother(wins) {
+	let cur = await browser.windows.getCurrent({})
+	let other = cur.id === wins[0].id ? wins[1] : wins[0]
+	let [tab] = await browser.tabs.query({active: true, currentWindow: true})
+	await browser.tabs.move(tab.id, {windowId: other.id, index: -1})
+}
+
+async function moveTabToNew() {
+	let [tab] = await browser.tabs.query({active: true, currentWindow: true})
+	await browser.windows.create({tabId: tab.id})
+}
+
+async function moveFromTabWin() {
+	let [tab] = await browser.tabs.query({active: true, windowId: tabWin})
+	let cur = await browser.windows.getCurrent({})
+	await browser.tabs.move(tab.id, {windowId: cur.id, index: -1})
 }
 
 async function visibleWindows() {
-	return new Promise(resolve => {
-		chrome.windows.getAll({windowTypes: ["normal"]}, wins => {
-			resolve(wins.filter(w => w.state !== "minimized"))
-		})
-	})
-}
-
-async function newWindow(tid) {
-	return new Promise(resolve => {
-		chrome.windows.create({tabId: tid}, win => { resolve(win.id) })
-	})
-}
-
-function currentWin() {
-	return new Promise(resolve => {
-		chrome.windows.getCurrent({}, win => { resolve(win.id) })
-	})
-}
-
-function activeTab(wid) {
-	let arg = {active: true}
-	if (wid) {
-		arg.windowId = wid
-	} else {
-		arg.currentWindow = true
-	}
-	return new Promise(resolve => chrome.tabs.query(arg, tabs => { resolve(tabs[0]) }))
-}
-
-function queryTabs(arg) {
-	return new Promise(resolve => {
-		chrome.tabs.query(arg, tabs => { resolve(tabs) })
-	})
-}
-
-async function moveTabs() {
-	let wins = await visibleWindows()
-	if (tabWin === 0 && wins.length > 2) {
-		return false
-	}
-
-	let src = tabWin
-	tabWin = 0
-	chrome.browserAction.setBadgeText({text: ''})
-	let cur = await currentWin()
-	if (src === 0) {
-		src = cur
-	}
-
-	let dst = 0
-	if (src === cur) {
-		if (wins.length < 2) {
-			dst = 0
-		} else if (wins.length === 2) {
-			dst = src === wins[0].id ? wins[1].id : wins[0].id
-		} else {
-			dst = 0
-		}
-	} else {
-		dst = cur
-	}
-
-	let tabs = await queryTabs({highlighted: true, windowId: src})
-	let active = await activeTab(src)
-	let tids = tabs.map(x => x.id)
-	if (dst === 0) {
-		dst = await newWindow(tids[0])
-		tids.shift()
-	}
-	if (tids.length > 0) {
-		let pinned = await queryTabs({pinned: true, windowId: dst})
-		chrome.tabs.move(tids, {windowId: dst, index: pinned.length})
-	}
-	chrome.tabs.update(active.id, {active: true})
-	chrome.windows.update(dst, {focused: true})
-	return true
+	let wins = await browser.windows.getAll({windowTypes: ["normal"]})
+	return wins.filter(w => w.state !== 'minimized')
 }
 
 async function setTabWin() {
-	tabWin = await currentWin()
-	chrome.browserAction.setBadgeText({text: "!"})
+	let cur = await browser.windows.getCurrent({})
+	tabWin = cur.id
+	await browser.browserAction.setBadgeText({text: "!"})
 }
 
 function init() {
-	chrome.browserAction.onClicked.addListener(handleMoveTabs)
+	browser.browserAction.onClicked.addListener(handleMoveTabs)
 }
 
 init()
