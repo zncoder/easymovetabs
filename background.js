e2353898352ba8e5a6cfab28ee52614dbe8f9d3b
Bin Zhang <bin.zhang@databricks.com>
2024-10-22 14:53:54 -0700

    upgrade to manifest v3

--- background.js
+++ background.js
@@ -3,7 +3,7 @@ let tabWin = 0
 async function handleMoveTabs() {
 	if (tabWin !== 0) {
 		try {
-			await browser.windows.get(tabWin)
+			await chrome.windows.get(tabWin)
 		} catch (e) {
 			tabWin = 0
 		}
@@ -14,7 +14,7 @@ async function handleMoveTabs() {
 	} else {
 		await moveFromTabWin()
 		tabWin = 0
-		await browser.browserAction.setBadgeText({text: ""})
+		await chrome.action.setBadgeText({text: ""})
 	}
 }
 
@@ -30,36 +30,36 @@ async function moveOrSetTabWin() {
 }
 
 async function moveTabToAnother(wins) {
-	let cur = await browser.windows.getCurrent({})
+	let cur = await chrome.windows.getCurrent({})
 	let other = cur.id === wins[0].id ? wins[1] : wins[0]
-	let [tab] = await browser.tabs.query({active: true, currentWindow: true})
-	await browser.tabs.move(tab.id, {windowId: other.id, index: 0})
+	let [tab] = await chrome.tabs.query({active: true, currentWindow: true})
+	await chrome.tabs.move(tab.id, {windowId: other.id, index: 0})
 }
 
 async function moveTabToNew() {
-	let [tab] = await browser.tabs.query({active: true, currentWindow: true})
-	await browser.windows.create({tabId: tab.id})
+	let [tab] = await chrome.tabs.query({active: true, currentWindow: true})
+	await chrome.windows.create({tabId: tab.id})
 }
 
 async function moveFromTabWin() {
-	let [tab] = await browser.tabs.query({active: true, windowId: tabWin})
-	let cur = await browser.windows.getCurrent({})
-	await browser.tabs.move(tab.id, {windowId: cur.id, index: 0})
+	let [tab] = await chrome.tabs.query({active: true, windowId: tabWin})
+	let cur = await chrome.windows.getCurrent({})
+	await chrome.tabs.move(tab.id, {windowId: cur.id, index: 0})
 }
 
 async function visibleWindows() {
-	let wins = await browser.windows.getAll({windowTypes: ["normal"]})
+	let wins = await chrome.windows.getAll({windowTypes: ["normal"]})
 	return wins.filter(w => w.state !== 'minimized')
 }
 
 async function setTabWin() {
-	let cur = await browser.windows.getCurrent({})
+	let cur = await chrome.windows.getCurrent({})
 	tabWin = cur.id
-	await browser.browserAction.setBadgeText({text: "!"})
+	await chrome.action.setBadgeText({text: "!"})
 }
 
 function init() {
-	browser.browserAction.onClicked.addListener(handleMoveTabs)
+	chrome.action.onClicked.addListener(handleMoveTabs)
 }
 
 init()
